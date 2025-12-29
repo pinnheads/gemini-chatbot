2126ca7417a5cdb3d38ac527565c97805b48fe0e
Utsav Deep <utsavdeep01@gmail.com>
2025-12-29 17:52:26 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+from google import genai
+from google.genai import types
+
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError(
+        "ERROR: Couldn't load api_key!. Please check the .env file."
+    )
+
+client = genai.Client(api_key=api_key)
+parser = argparse.ArgumentParser(description='Chatbot')
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+messages = [types.Content(
+    role="user", parts=[types.Part(text=args.user_prompt)])]
+
+answer = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents=messages
+)
+
+if answer.usage_metadata is None:
+    raise RuntimeError(
+        "ERROR: There was an error when connecting to gemini api!. Please try again in some time."
+    )
+
+
+print(f"User Prompt: {args.user_prompt}")
+print(f"Prompt tokens: {answer.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {answer.usage_metadata.candidates_token_count}")
+print(f"Response: {answer.text}")
